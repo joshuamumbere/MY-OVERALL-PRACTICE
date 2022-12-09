b6a2ed994de5b2ab985e426bc18b1ca4b541f2a2
joshuamumbere <joshuamumbere71@gmil.com>
2022-12-09 09:32:29 +0300

    updates

---
+++ PRACTICE/DSAjava/Arryshit.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Arryshit {
+    public static void main(String[] args) {
+        int Ages[]=int[10];
+        Ages[0]=10;
+        Ages[1]=20;
+        Ages[2]=30;
+        Ages[3]=40;
+        Ages[4]=50;
+        Ages[5]=60;
+        Ages[6]=70;
+        int x=0;
+        for(x=0;x<10;x++){
+            System.out.println(Ages[3]);
+        }
+            }
+}
