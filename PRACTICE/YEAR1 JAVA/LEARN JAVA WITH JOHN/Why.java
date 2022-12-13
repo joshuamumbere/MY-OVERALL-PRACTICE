d48425fc23a15111b4f345f7581378423ba74131
joshuamumbere <joshuamumbere71@gmil.com>
2022-12-13 15:06:25 +0300

    updates

---
+++ PRACTICE/YEAR1 JAVA/LEARN JAVA WITH JOHN/Why.java
@@ -0,0 +1,18 @@
+public class Why {
+    int Number=10;
+    public void Education(){
+        System.out.println("I would go back to school for as many as " +Number+" times");
+        class Whying2{
+            int retake=2;
+        public void Units(){
+            System.out.println("The highest number of retakes should be " +retake +"times");
+        }
+        }
+        Whying2 x=new Whying2();
+        x.Units();
+    }
+    public static void main(String[] args) {
+        Why p =new Why();
+        p.Education();
+    }
+}
