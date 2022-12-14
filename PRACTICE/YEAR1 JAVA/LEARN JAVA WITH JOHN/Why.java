a316665739467732fd4b15cf123dca961ad21f68
joshuamumbere <joshuamumbere71@gmil.com>
2022-12-14 12:18:42 +0300

    updates

--- PRACTICE/YEAR1 JAVA/LEARN JAVA WITH JOHN/Why.java
+++ PRACTICE/YEAR1 JAVA/LEARN JAVA WITH JOHN/Why.java
@@ -1,18 +1,27 @@
+import java.util.Scanner;
+
 public class Why {
-    int Number=10;
-    public void Education(){
+    // int Number=10;
+    public void Education(int Number,int retake){
         System.out.println("I would go back to school for as many as " +Number+" times");
         class Whying2{
-            int retake=2;
-        public void Units(){
+            // int retake=2;
+        public void Units(int retake){
             System.out.println("The highest number of retakes should be " +retake +"times");
+            System.out.println(Number);
         }
         }
         Whying2 x=new Whying2();
-        x.Units();
+        x.Units(retake);
     }
     public static void main(String[] args) {
+        // System.out.println(retake);
         Why p =new Why();
-        p.Education();
+        Scanner y=new Scanner(System.in);
+        System.out.println("enter number :");
+        int num=y.nextInt();
+        System.out.println("enter retake :");
+        int ret=y.nextInt();
+        p.Education(num,ret);
     }
 }
