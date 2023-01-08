c7e2104e53488b05ff17806ab4581ba94d852aaa
joshuamumbere <joshuamumbere71@gmil.com>
2023-01-08 11:12:50 +0300

    updates

---
+++ PRACTICE/python practice/sample.py
@@ -0,0 +1,28 @@
+#the following are some resources that i will need to use 
+"""
+while True:
+    if y in price:
+         if y<50:
+            #  print("Change Owed :" ,ActualChange)
+             print("Amount Due :" ,AmountDue)
+        # print("The price is accepted")
+        # print(list1.index(y))
+        # print(y)
+            #  break
+         elif y==50:
+             print("Change Owed :" ,ActualChange)
+             break
+        # break
+       
+         else :
+            #  print("Amount Due :" ,AmountDue)
+            print("Change Owed :" ,ActualChange)
+            break
+
+       
+    break
+# else:
+        # print("The price is not accepted")
+        # break
+
+"""
--- PRACTICE/python practice/trial.py
+++ PRACTICE/python practice/trial.py
@@ -2,3 +2,5 @@ name=input("whats your name:")
 school=input("whats your school:")
 
 print(f"hello , {name} ,{school}")
+age=int(input("whats your age:"))
+print("you are",age,"years old")
